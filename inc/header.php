9312f5478a2c4506f240b8c2e46381b6478f424a
Pham Chi Cuong <Pham16698@gmail.com>
2023-03-16 01:00:42 +0700

    Update style for header

--- inc/header.php
+++ inc/header.php
@@ -15,12 +15,11 @@
     <meta name="author" content="">
 
 	<title>Clothes Word</title>
+	<link rel="stylesheet" type="text/css" href="css/header.css" rel="preload">
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-
-
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
  	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<link rel="stylesheet" type="text/css" href="css/cart.css">
-    
+   
 </head>
 <body>
