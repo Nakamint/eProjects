8155a88327dfe3a2c279b82494df068e8398cc46
Pham Chi Cuong <Pham16698@gmail.com>
2023-03-22 00:57:04 +0700

    Style all website

--- inc/footer.php
+++ inc/footer.php
@@ -3,5 +3,17 @@
 	<script type="text/javascript" src="js/bootstrap.min.js"></script>
 	<script type="text/javascript" src="js/function.js"></script>
 	<script type="text/javascript" src="js/main.js"></script>
+<script>
+	window.onload = function (){
+	custompriceOther()
+};
+function custompriceOther() {
+	$('.custom-price').each(function() {
+		var money = +$(this).text();
+		var moneyDots = money.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1.");
+		$(this).text('  ' + moneyDots + ' VND ');
+	});
+};
+</script>
 </body>
 </html>
